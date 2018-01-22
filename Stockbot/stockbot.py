ffbf3ce0ca0e37a9aa2c8cdc0a0b43c574283913
Sigve Mølmann <smo077@post.uit.no>
2018-01-22 02:11:26 +0100

    V0.3 - Formatting
    Added docstrings and split methods into smaller, slightly more readable chunks. No major changes to functionality. Also updated sample.csv.

--- Stockbot/stockbot.py
+++ Stockbot/stockbot.py
@@ -17,33 +17,46 @@ class Stock():
                     -   A tuple of the latest action (buy or sell) and the date
 
     Methods:
-        init_data   -   Gets a Pandas DataFrame with relevant information about the stock and saves it to a csv file with path from Stock.path
+        init_data   -   Gets a Pandas DataFrame with relevant information about the stock and saves it to a csv file with path from Stock.path.
         init_data_csv
-                    -   Gets a Pandas DataFrame from a csv file with the path from Stock.path
-        update_data -   Appends new data to existing data. Also saves to local csv
-        splot       -   [avg]: Array of averages that should be plotted. If averages have not been computed, do so and add them to data
-        get_avg     -   interval: The number of days that should be taken average of including present day
-                        Finds the average closing price over 'interval' number of days and adds it to daily_data.avg_price
-        view_data   -   Prints the contents of Stock.data to the console
-        rule_buy    -   Returns the latest index where the rx-avg is lower than ry-avg, and rx-avg is lower than daily closing price
-        rule_sell   -   Returns the latest index where the rx-avg is higher than ry-avg, and rx-avg is higher than daily closing price
+                    -   Gets a Pandas DataFrame from a csv file with the path from Stock.path.
+        update_data -   *TODO* Appends new data to existing data. Also saves to local csv.
+        splot       -   Plots a graph of closing price and closing averages specified in 'avg'.
+        get_avg     -   Finds the average closing price over 'avg_interval' number of days and adds a column to Stock.data.
+        print_data  -   Prints the Stock.data to the console.
+        create_avg  -   Creates the
+        do_rule_buy -   Asserts if a buy-signal should be triggered.
+        rule_buy    -   Returns the latest index where Stock.do_rule_buy() returns True.
+        do_rule_sell-   Asserts if a sell-signal should be triggered.
+        rule_sell   -   Returns the latest index where Stock.do_rule_sell() returns True.
 
     """
-    def __init__(self, symbol):
+    def __init__(self, symbol, path="C:\\Stockbot\\Stocks", num_days=1000):
         """
+        params:
+            symbol   - (String) The unique character combination indicating a certain share.
+            path     - (String) Default "C:\\Stockbot\\Stocks". The path directory where the Stocks related csv will be stored.
+            num_days - (Int) Default 1000. The number of days for data gathering including closing days.
+        returns:
+            None
+        Initializing method.
         """
         self.symbol = symbol.upper()
-        # self.name = self.get_name()
         self.path = "C:\\Stockbot\\Stocks\\{s}.csv".format(s=self.symbol)
-        # self.data = self.init_data()
+        # self.data = self.init_data(num_days)
         self.data = self.init_data_csv()
         self.last_action = (0,0)  # Tuple of buy/sell and date
 
-    def init_data(self):
+    def init_data(self, num_days=1000):
         """
+        params:
+            num_days - (Int) Default 1000. Number of days to fetch data for, including closing days
+        returns:
+            (pandas.DataFrame) A DataFrame for the last num_days days' worth of stock data. Values [ High, Low, Close, Volume ] are kept.
+        Fetches data from Yahoo Finance using pandas_datareader the last num_days days. Writes the resulting csv to path as {symbol}.csv which is subsecuently is read and returned.
         """
         end = datetime.today()
-        start = end - timedelta(days=1000)
+        start = end - timedelta(days=num_days)
 
         df = web.DataReader(self.symbol, "yahoo", start, end)
         df.to_csv(path_or_buf=self.path,columns=["High","Low","Close","Volume"])
@@ -52,26 +65,55 @@ class Stock():
 
     def init_data_csv(self):
         """
+        params:
+            None
+        returns:
+            (pandas.DataFrame) A DataFrame read from the csv stored in Stock.path.
+        Fetches data from a csv stored in Stock.path.
         """
         return pd.read_csv(self.path)
 
     def update_data(self):
+        """
+        *TODO* Appends new data to existing data. Also saves to local csv.
+        """
         pass
 
     def splot(self,avg=None):
         """
+        params:
+            avg - (List of Ints) Defualt None. If unchanged, plot only closing prices. Plot averages specified in avg.
+        returns:
+            None.
+        Plots a graph of closing price and closing averages specified in 'avg'.
         """
-        if avg == None:
-            self.data.plot(x=self.data.index,y="Close",grid=True,ylim=(max(self.data["Close"]*1.1),min(self.data["Close"])*0.9))
-        else:
-            self.data.plot(x=self.data.index,y=avg,grid=True,ylim=(max(self.data["Close"]*1.1),min(self.data["Close"])*0.9))
+        avgs = ["Close"]
+        for avg_interval in avg:
+            self.create_avg(avg_interval)
+            avgs.append("avg_{avg_interval}".format(avg_interval=avg_interval))
+        self.data.plot(x=self.data.index, y=avgs, grid=True, ylim=(max(self.data["Close"]*1.1),min(self.data["Close"])*0.9))
         plt.gca().invert_yaxis()
         plt.show()
 
-    def get_avg(self,n):
+    def print_data(self):
         """
+        params:
+            None.
+        returns:
+            None.
+        Prints the Stock.data to the console.
         """
-        col = "avg_{n}".format(n=n)
+        print("{s}\n{p}\n{d}".format(s=self.symbol,p=self.path,d=self.data))
+
+    def get_avg(self,avg_interval):
+        """
+        params:
+            avg_interval - (Int) The interval of days that should be averaged.
+        returns:
+            (pandas.DataFrame) Stock.data including the newly created average column.
+        Finds the average closing price over 'avg_interval' number of days and adds a column to Stock.data.
+        """
+        col = "avg_{avg_interval}".format(avg_interval=avg_interval)
 
         prices = self.data["Close"]
         dates = self.data["Date"]
@@ -80,14 +122,14 @@ class Stock():
         d = deque()
         for idx, price in enumerate(prices):
             if not np.isnan(price):
-                if len(d) < n:
+                if len(d) < avg_interval:
                     d.append(price)
                 else:
                     d.popleft()
                     d.append(price)
 
-                if len(d) == n:
-                    avg = sum(d)/n
+                if len(d) == avg_interval:
+                    avg = sum(d)/avg_interval
                     self.data.loc[idx, col] = avg
                 else:
                     self.data.loc[idx, col] = np.nan
@@ -95,64 +137,102 @@ class Stock():
                 self.data.loc[idx, col] = np.nan
         return self.data
 
-    def view_data(self):
+    def create_avg(self, avg_interval):
         """
+        params:
+            avg_interval - (Int) The interval of days that should be averaged.
+        returns:
+            (pandas.DataFrame) Stock.data including the newly created average column, if any.
+        Finds the average closing price over 'avg_interval' number of days and adds a column to Stock.data if the column does not already exsists.
         """
-        print("{s}  {n}\n{p}\n{d}".format(n=self.name,s=self.symbol,p=self.path,d=self.data))
+        if not (avg_interval in self.data.columns):
+            df = self.get_avg(avg_interval)
+        return df
 
-    def rule_buy(self, rx, ry):
+    def do_rule_buy(self, idx, col_x, col_y):
         """
+        params:
+            idx   - (Int) The index of Stock.data that should be examined.
+            col_x - (String) Name of the first column for comparison.
+            col_y - (String) Name of the second column for comparison.
+        returns:
+            (Boolean) The evaluation of whether or not it would be recommended to buy this Stock based on the following rule: (closing_price > val_x and val_x < val_y).
+        Asserts if a buy-signal should be triggered.
         """
-        col_x = "avg_{x}".format(x=rx)
-        col_y = "avg_{y}".format(y=ry)
+        price = self.data.loc[idx, "Close"]
+        avg_x = self.data.loc[idx, col_x]
+        avg_y = self.data.loc[idx, col_y]
 
-        # Assert if columns exist. If not get them
-        if not (col_x in self.data.columns):
-            self.get_avg(rx)
-        if not (col_y in self.data.columns):
-            self.get_avg(ry)
+        if price > avg_x and avg_x < avg_y:
+            return True
+        else:
+            return False
+
+    def rule_buy(self, x, y):
+        """
+        params:
+            x - (Int) The first average to be compared.
+            y - (Int) The second average to be compared.
+        returns:
+            (Int) The latest index where a buy signal was triggered.
+        Returns the latest index where Stock.do_rule_buy() returns True.
+        """
+        col_x = "avg_{x}".format(x=x)
+        self.create_avg(x)
+
+        col_y = "avg_{y}".format(y=y)
+        self.create_avg(y)
 
         for idx in reversed(self.data.index):
-            c = self.data.loc[idx, "Close"]
-            x = self.data.loc[idx, col_x]
-            y = self.data.loc[idx, col_y]
-            if (c > y) and (x < y):
-                print("Date: {d} (idx={i}), C: {c}, X: {x}, Y: {y}".format(d=self.data.loc[idx, "Date"], i=idx, c=c, x=x, y=y))
-                # return idx
+            if self.do_rule_buy(idx, col_x, col_y):
+                return idx
 
-    def rule_sell(self, rx, ry):
+    def do_rule_sell(self, idx, col_x, col_y):
         """
+        params:
+            idx   - (Int) The index of Stock.data that should be examined.
+            col_x - (String) Name of the first column for comparison.
+            col_y - (String) Name of the second column for comparison.
+        returns:
+            (Boolean) The evaluation of whether or not it would be recommended to sell this Stock based on the following rule: (closing_price < val_x and val_x > val_y).
+        Asserts if a sell-signal should be triggered.
         """
-        col_x = "avg_{x}".format(x=rx)
-        col_y = "avg_{y}".format(y=ry)
+        price = self.data.loc[idx, "Close"]
+        avg_x = self.data.loc[idx, col_x]
+        avg_y = self.data.loc[idx, col_y]
 
-        # Assert if columns exist. If not get them
-        if not (col_x in self.data.columns):
-            self.get_avg(rx)
-        if not (col_y in self.data.columns):
-            self.get_avg(ry)
+        if price < avg_x and avg_x > avg_y:
+            return True
+        else:
+            return False
+
+    def rule_sell(self, x, y):
+        """
+        params:
+            x - (Int) The first average to be compared.
+            y - (Int) The second average to be compared.
+        returns:
+            (Int) The latest index where a sell signal was triggered.
+        Returns the latest index where Stock.do_rule_sell() returns True.
+        """
+        col_x = "avg_{x}".format(x=x)
+        self.create_avg(x)
+
+        col_y = "avg_{y}".format(y=y)
+        self.create_avg(y)
 
         for idx in reversed(self.data.index):
-            c = self.data.loc[idx, "Close"]
-            x = self.data.loc[idx, col_x]
-            y = self.data.loc[idx, col_y]
-            if (c < x) and (x > y):
-                print("Date: {d} (idx={i}), C: {c}, X: {x}, Y: {y}".format(d=self.data.loc[idx, "Date"], i=idx, c=c, x=x, y=y))
-                # return idx
+            if self.do_rule_sell(idx, col_x, col_y):
+                return idx
 
-def simulate_market(stock, start_money, avg=(2,10), lst=None):
+def simulate_market(stock, start_money, avg=(2,10)):
     """ avg  - the lowest and highest averages to be examined
     """
+    # Create all averages from start through end intervals
     start, end = avg
-
-    # Get all averages from start to, and including, end intervals
-    print("Calculating averages...")
     for x in range(start, end + 1):
         col_x = "avg_{x}".format(x=x)
-
-        # Assert if Stock has y-avg. If not, get it
-        if not (col_x in stock.data.columns):
-            stock.get_avg(x)
+        stock.create_avg(x)
 
     # Variables to contain logging results
     max_money = 0
@@ -160,55 +240,37 @@ def simulate_market(stock, start_money, avg=(2,10), lst=None):
     max_num_purchases = 0
 
     # Loop across averages and find the optimal intervals, only use y where y > x + 1
-    print("Calculating optimmal avgerages...")
     for x in range(start, end):
         col_x = "avg_{x}".format(x=x)
         gen = (y for y in range(start + 1, end + 1) if y > x + 1)
         for y in gen:
-            # Simulate buying and selling for x- and y-avg
             # Initializing variables
-            money = start_money
-            num_bought = 0
-            num_purchases = 0
-            mode = "buy"
-            idx = max([x,y])
-            idx_max = stock.data.last_valid_index()
+            money, num_bought, num_purchases, mode = start_money, 0, 0, "buy"
+            idx, idx_max = y, stock.data.last_valid_index()
             col_y = "avg_{y}".format(y=y)
 
-            while idx <= idx_max:
-                price = stock.data.loc[idx, "Close"]
-                avg_x = stock.data.loc[idx, col_x]
-                avg_y = stock.data.loc[idx, col_y]
+            for idx in range(0, idx_max + 1):
+                # Want to buy
+                if mode == "buy" and stock.do_rule_buy(idx, col_x, col_y):
+                    mode = "sell"
+                    price = stock.data.loc[idx, "Close"]
+                    num_bought, money = money / price, 0
+                    num_purchases += 1
 
-                # Looking to buy
-                if mode == "buy":
-                    # Buy signal
-                    if (price > avg_y) and (avg_x < avg_y):
-                        print("BUY: {idx}  {p}".format(idx=idx, p=price))
-                        mode = "sell"
-                        num_bought = money / price
-                        money = 0
-                        num_purchases +=1
-
-                # Looking to sell
-                if mode == "sell":
-                    # Sell signal
-                    if (price < avg_x) and (avg_x > avg_y):
-                        print("SELL: {idx}  {p}".format(idx=idx, p=price))
-                        mode = "buy"
-                        money = num_bought * price
-                        num_bought = 0
-                        num_purchases += 1
-                # Increment idx
-                idx += 1
+                # Want to sell
+                if mode == "sell" and stock.do_rule_sell(idx, col_x, col_y):
+                    mode = "buy"
+                    price = stock.data.loc[idx, "Close"]
+                    money, num_bought = num_bought * price, 0
+                    num_purchases += 1
 
             # Finally sell all to see profit
             money = num_bought * price
 
-            # Printing result of x-, y-avg
-            print("Avg: {x}  {y}  {t}\nGross: {profit}  ({diff})\n\n\n".format(x=x, y=y, t=num_purchases, profit=round(money/start_money,3), diff=round(money-start_money,3)))
+            # # Printing result of x-, y-avg
+            # print("Avg: {x}  {y}  {t}\nGross: {profit}  ({diff})\n\n\n".format(x=x, y=y, t=num_purchases, profit=round(money/start_money,3), diff=round(money-start_money,3)))
 
-            # Logging max and min values
+            # Logging max values
             if money >= max_money and num_purchases > 1:
                 max_money = money
                 max_avg = (x, y)
@@ -217,31 +279,23 @@ def simulate_market(stock, start_money, avg=(2,10), lst=None):
     # Print logs
     maxx, maxy = max_avg
     print("MAX:: {p}%  ({x}, {y}). Num {n}".format(p=round(max_money/start_money*100,3), x=maxx, y=maxy, n=max_num_purchases))
-# End simulate_market method
 
 
 if __name__ == "__main__":
-    print("Getting data...")
-    test_stock = Stock("GSF.OL")
-    test_stock.get_avg(7)
-    test_stock.get_avg(25)
-    # test_stock.view_data()
-    print("BUY")
-    test_stock.rule_buy(45, 48)
-    print("SELL")
-    test_stock.rule_sell(45, 48)
-
-    # simulate_market(test_stock, 10000, (5,50))
-    # test_stock.splot(["Close", "avg_5", "avg_15"])
-    test_stock.splot(["Close", "avg_7", "avg_25"])
+    test_stock = Stock("AMZN")
+    # test_stock.get_avg(2)
+    # test_stock.print_data()
+    # test_stock.rule_buy(3, 4)
+    # test_stock.rule_sell(5, 6)
+    # simulate_market(test_stock, 10000, (7,10))
+    # test_stock.splot([11, 12])
 
 
-
-
-
-
-
-
-
-
-# Whitespace
+"""
+TODO:
+Retry fetching data from web
+Write the Stock.update_data() method
+Create a proper test method
+Check Stock.init_csv() in case no csv in Stock.path
+Create notification system that provides insigh whether or not it recommends to buy/sell
+"""
